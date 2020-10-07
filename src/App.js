c0a870a00f0b18b14dfda00263625f2a51c3c169
Elronia <zar.catherine@gmail.com>
2020-10-07 15:40:47 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends React.Component{
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={data}/>
       </>
     );
   }
