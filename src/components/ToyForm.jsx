6da43a575aa107f106edbf1418fb0983b5be2184
nisayana <nimaevasb@gmail.com>
2021-03-07 22:14:06 -0500

    Done.

--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,22 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class ToyForm extends Component {
+function ToyForm(props) {
 
-  render() {
+  console.log(props)
+
+  let [name, setToyName] = useState("")
+  let [image, setToyImage] = useState("")
+  let [likes, setToyLikes] = useState(0)
+
+  let handleSubmit = (evt) => {
+    evt.preventDefault()
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-type": "Application/json"
+      },
+      body: JSON.stringify({
+        name,
+        image,
+        likes
+      })
+    })
+    .then(res => res.json())
+    .then(newToy => {
+      props.addAToy(newToy)
+      setToyName("")
+      setToyImage("")
+    })
+  }
+
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={handleSubmit}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" name="name" id="toy_name" value={name} onChange={e => {setToyName(e.target.value)}} placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" name="image" id="toy_image" value={image} onChange={e => {setToyImage(e.target.value)}} placeholder="Enter a toy's image URL..." className="input-text"/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
       </div>
     );
-  }
-
 }
 
 export default ToyForm;
